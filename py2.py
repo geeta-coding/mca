8de690785b276fd217f1d0cc04139f233f320298
geeta <geeta@gmail.com>
2026-03-13 00:25:13 +0530

    task 2 complete

---
+++ py2.py
@@ -0,0 +1,25 @@
+# task 2 :Assign string, int, and float variables.
+#  string
+first_name = "Geeta"
+middle_name  = "dnyaneshwar"
+last_name = "raut"
+
+print(f"welcome",first_name,middle_name,last_name)
+
+# integer anf float
+student_marks = 80
+percentage = 80.60
+if (student_marks>=90):
+    print("A grade") 
+elif (student_marks>=70):
+    print("B grade")
+else:
+    print("fail")
+
+if percentage>=80:
+    print("first class")
+elif percentage>=70:
+    print("secound class")
+else:
+    print("fail")
+
